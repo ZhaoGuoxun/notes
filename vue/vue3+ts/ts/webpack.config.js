54d32649799727499e985cd5936934f167fa58a3
zgx <573087523@qq.com>
2021-08-30 23:02:14 +0800

    ts更新

---
+++ vue/vue3+ts/ts/webpack.config.js
@@ -0,0 +1,95 @@
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
+
+const isProd = process.env.NODE_ENV === 'production' // 是否生产环境
+
+function resolve(dir) {
+  return path.resolve(__dirname, '..', dir)
+}
+
+module.exports = {
+  mode: isProd ? 'production' : 'development',
+  entry: {
+    app: './src/main.ts'
+  },
+
+  output: {
+    path: resolve('dist'),
+    filename: '[name].[contenthash:8].js'
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: [
+                [
+                  "@babel/preset-env",
+                  {
+                    // 要兼容的目标浏览器及版本
+                    targets: {
+                      "chrome": "58",
+                      "ie": "11"
+                    },
+                    //指定corejs的版本（根据package.json中的版本，只写整数）
+                    "corejs": "3",
+                    //使用corejs的方式 "usage"  表示按需加载
+                    "useBuiltIns": "usage"
+                  }
+                ]
+              ]
+            }
+          },
+          'ts-loader'
+        ],
+        exclude: /node_modules/
+      }, {
+        test: /\.less$/,
+        use: [
+          "style-loader",
+          "css-loader",
+          {
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins: [
+                  ["postcss-preset-env", {
+                    browsers: 'last 2 versions'
+                  }]
+                ]
+              }
+            }
+          },
+          "less-loader",
+        ]
+      }
+    ]
+  },
+
+  plugins: [
+    new CleanWebpackPlugin({
+    }),
+
+    new HtmlWebpackPlugin({
+      template: './public/index.html'
+    })
+  ],
+
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js']
+  },
+
+  devtool: 'eval',
+
+  devServer: {
+    host: 'localhost', // 主机名
+    stats: 'errors-only', // 打包日志输出输出错误信息
+    port: 8080,
+    open: true
+  },
+}
